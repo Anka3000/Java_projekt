bc9c1e53f870ec422c00365d80ac6e6cc7be6bea
Anka3000 <94915856+Anka3000@users.noreply.github.com>
2023-05-06 21:22:36 +0200

    Add files via upload

---
+++ INSERT.sql
@@ -0,0 +1,5 @@
+INSERT INTO PLYTY_DVD VALUES (1, 'Nocne chwile', 'Pawel Nowak', '2015-04-20', 'obyczajowy', 15.2);
+INSERT INTO PLYTY_DVD VALUES (2, 'Podejrzane wizje', 'Marek Witkowski', '2018-12-03', 'thriller', 22.5);
+INSERT INTO PLYTY_DVD VALUES (3, 'Bialy szum – kilka wariantow', 'Maria Golec', '2020-06-30', 'film relaksujacy', 19.95);
+INSERT INTO PLYTY_DVD VALUES (4, 'Stairs to heaven', 'Kim Smith', '2022-07-05', 'akcja', 40.95);
+INSERT INTO PLYTY_DVD VALUES (5, 'Yes', 'Just Marian', '2016-10-15', 'komedia', 34.9);
