186d1458bb43fd2be6a5c87b3a4b3c5d165adad3
Anka3000 <94915856+Anka3000@users.noreply.github.com>
2023-05-06 22:00:06 +0200

    Update INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -1,11 +1,19 @@
+--tabela PLYTY_DVD
 INSERT INTO PLYTY_DVD VALUES (1, 'Nocne chwile', 'Pawel Nowak', '2015-04-20', 'obyczajowy', 15.2);
 INSERT INTO PLYTY_DVD VALUES (2, 'Podejrzane wizje', 'Marek Witkowski', '2018-12-03', 'thriller', 22.5);
 INSERT INTO PLYTY_DVD VALUES (3, 'Bialy szum – kilka wariantow', 'Maria Golec', '2020-06-30', 'film relaksujacy', 19.95);
 INSERT INTO PLYTY_DVD VALUES (4, 'Stairs to heaven', 'Kim Smith', '2022-07-05', 'akcja', 40.95);
 INSERT INTO PLYTY_DVD VALUES (5, 'Yes', 'Just Marian', '2016-10-15', 'komedia', 34.9);
-
+--tabela KLIENCI
 INSERT INTO KLIENCI VALUES (1, 'Jan', 'Kowalski', 'Polna 15 - Zachecien', '111 111 111', 'JKol@interia.pl');
 INSERT INTO KLIENCI VALUES (2, 'Marianna', 'Wrona', 'Warszawska 8/30 - Morszyn', '000 999 555', 'Mari5@onet.pl');
 INSERT INTO KLIENCI VALUES (3, 'Julia', 'Gawron', 'Szkolna 16 - Polin', '999 888 777', 'Julia.Gawron@gmai.com');
 INSERT INTO KLIENCI VALUES (4, 'Nadia', 'Malinowska', 'Hoza 77/6 - Zachecien', '123 456 789', 'MalinyGit@wp.pl');
 INSERT INTO KLIENCI VALUES (5, 'Bartosz', 'Kwiecien', 'Zamkowa 15/20 - Morszyn', '001 002 003', 'KvetenB@poczta.pl');
+--tabela RACHUNKI
+INSERT INTO RACHUNKI VALUES (1, '2021-05-20', 19.2, 5);
+INSERT INTO RACHUNKI VALUES (2, '2022-12-15', 34.9, 4);
+INSERT INTO RACHUNKI VALUES (3, '2023-01-23', 19.95, 3);
+INSERT INTO RACHUNKI VALUES (4, '2023-03-15', 40.95, 2);
+INSERT INTO RACHUNKI VALUES (5, '2023-05-06', 22.5, 1);
+--tabela 
